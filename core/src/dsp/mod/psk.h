34e52f5f6f193cec088d62c8e0c18931cb778393
KubaPro010 <kuba.pro010@gmail.com>
2024-04-18 17:16:20 +0200

    remove more files

--- core/src/dsp/mod/psk.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-#include "../multirate/rrc_interpolator.h"
-
-namespace dsp::mod {
-    // TODO: Check if resample before RRC is better than using the RRC taps as a filter (bandwidth probably not correct for alias-free resampling)
-    typedef multirate::RRCInterpolator<complex_t> PSK;
-}
